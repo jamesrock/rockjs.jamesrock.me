1bf2b1ddf7770a7b3960dcb8b901077b45434c21
James Rock <hello@jamesrock.me>
2017-05-31 18:01:36 +0100

    removed src

--- js/ExampleClass.js
+++ js/ExampleClass.js
@@ -1,10 +1,14 @@
 var Person = ROCK.Object.extend({
   constructor: function Person(id, forename, surname) {
+
     this.id = id;
     this.forename = forename;
     this.surname = surname;
+
   },
   getFullName: function() {
+
     return [this.forename, this.surname].join(" ");
+    
   }
 });
--- src/5.0/ROCK.Class.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-	var Class = ROCK.Class = ROCK.createClass(Object, function Class() {
-
-		throw("An instance of ROCK.Class cannot be created. Please use ROCK.Class.extend();");
-
-	});
-
-})();
